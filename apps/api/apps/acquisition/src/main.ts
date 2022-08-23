d7746cc089922daef87030eeece0100fb9f78761
Erick Gomez <erick@eendlabs.com>
2022-08-23 22:23:30 +0000

    feat: use fastify instead of express

--- apps/api/apps/acquisition/src/main.ts
+++ apps/api/apps/acquisition/src/main.ts
@@ -1,19 +1,28 @@
 import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppConfig } from '@tenet/config';
 import { AcquisitionModule } from './acquisition.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AcquisitionModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AcquisitionModule,
+    new FastifyAdapter(),
+  );
 
-  const config = app.get<AppConfig>(ConfigService);
+  const config = app.get(ConfigService);
+  const appConfig = config.get<AppConfig>('app');
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: config.defaultVersion,
+    defaultVersion: appConfig.defaultVersion,
   });
-  app.setGlobalPrefix(config.globalPrefix);
+  app.setGlobalPrefix(appConfig.globalPrefix);
 
-  await app.listen(config.port);
+  console.log(appConfig.port);
+  await app.listen(appConfig.port, '0.0.0.0');
 }
 bootstrap();
