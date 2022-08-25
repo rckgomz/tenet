0c8cd5a26c96871463e6b66887a45aea0f5f5fe9
Erick Gomez <erick@eendlabs.com>
2022-08-25 04:34:20 +0000

    feat: draft 1 person dto fields

--- apps/api/apps/acquisition/src/person/dto/create-person.dto.ts
+++ apps/api/apps/acquisition/src/person/dto/create-person.dto.ts
@@ -1,1 +1,35 @@
-export class CreatePersonDto {}
+import {
+  IsDate,
+  IsEmail,
+  IsMobilePhone,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreatePersonDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsNumberString()
+  @IsOptional()
+  ssn: string;
+
+  @IsDate()
+  @IsOptional()
+  dateOfBirth: Date;
+
+  @IsEmail()
+  primaryEmail: string;
+
+  @IsMobilePhone('us')
+  primaryPhoneNumber: string;
+
+  @IsOptional()
+  primaryAddress: {
+    addressLine1: string;
+  };
+}
