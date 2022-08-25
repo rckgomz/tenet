a963cd78d7c143f767cb3eeb828a1a68a9465c2f
Erick Gomez <erick@eendlabs.com>
2022-08-25 05:43:55 +0000

    feat: implement create method

--- apps/api/apps/acquisition/src/person/person.service.ts
+++ apps/api/apps/acquisition/src/person/person.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { plainToClassFromExist } from 'class-transformer';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { Person } from './entities/person.entity';
+import { Email } from './entities/email.entity';
+import { PhoneNumber } from './entities/phone.entity';
 
 @Injectable()
 export class PersonService {
+  constructor(
+    @InjectRepository(Person)
+    private readonly repo: Repository<Person>,
+    @InjectRepository(Email)
+    private readonly emailRepo: Repository<Email>,
+    @InjectRepository(PhoneNumber)
+    private readonly phoneRepo: Repository<PhoneNumber>,
+  ) {}
+
   create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+    const person = this.repo.create();
+    const newPerson = plainToClassFromExist(person, createPersonDto);
+
+    const email = this.emailRepo.create({
+      type: 'primary',
+      value: createPersonDto.primaryEmail.toLowerCase(),
+    });
+
+    const phone = this.phoneRepo.create({
+      type: 'primary',
+      value: createPersonDto.primaryPhoneNumber,
+    });
+
+    person.emails = [email];
+    person.phoneNumbers = [phone];
+
+    console.log(person);
+    return this.repo.save(newPerson);
   }
 
   findAll() {
-    return `This action returns all person`;
+    return this.repo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} person`;
+  findOne(id: string) {
+    return this.repo.findOneBy({ id });
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  update(id: string, updatePersonDto: UpdatePersonDto) {
+    return this.repo.update(id, updatePersonDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  remove(id: string) {
+    return this.repo.delete(id);
   }
 }
