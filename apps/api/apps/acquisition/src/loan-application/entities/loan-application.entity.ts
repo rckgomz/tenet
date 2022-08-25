1609c97ed68f6ad0c1017ad2315a7f5f66b1549f
Erick Gomez <erick@eendlabs.com>
2022-08-25 18:38:33 +0000

    feat: add base columns for loan application

--- apps/api/apps/acquisition/src/loan-application/entities/loan-application.entity.ts
+++ apps/api/apps/acquisition/src/loan-application/entities/loan-application.entity.ts
@@ -1,4 +1,5 @@
 import { BaseEntity } from '@tenet/database';
+import { IsCurrency } from 'class-validator';
 import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from 'typeorm';
 import { Product } from '../../product';
 import { LoanApplicationStatusType } from './loan-application-status.type';
@@ -14,6 +15,12 @@ export class LoanApplication extends BaseEntity {
     default: ['openned'],
   })
   status: LoanApplicationStatusType;
+
+  @IsCurrency()
+  amount: number;
+
+  @IsCurrency()
+  itemValue: number;
 
   @OneToOne(() => LoanOffer)
   @JoinColumn()
